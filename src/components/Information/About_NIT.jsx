64eec9f8221ef8d1f3312edb59049b3430ed59b0
kushbansal4974 <bkush535@gmail.com>
2025-02-17 10:14:28 +0530

    Initial commit

---
+++ src/components/Information/About_NIT.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function About_NIT() {
+  return (
+    <div>
+      <div className='about'>
+        <h2> ABOUT NIT UTTARAKHAND </h2>
+      </div>
+
+      <div className='hu'>
+        <div className='inside'>
+            <span className='line'>National Institute of Technology Uttarakhand (NIT Uttarakhand)</span> is one of the 31 NITs in India,<span className='line'>established in 2009 under the Ministry of Education</span> , Government of India. Located in Srinagar, Uttarakhand, it currently operates from a temporary campus while a permanent campus is being developed in Sumari. The institute offers undergraduate (B.Tech), postgraduate (M.Tech), and doctoral (Ph.D.) programs in various engineering and technology disciplines.NIT Uttarakhand focuses on academic excellence, research, and innovation, equipping students with technical knowledge and problem-solving skills. The institute has well-equipped laboratories, modern classrooms, and an active research environment. Despite infrastructure challenges due to its temporary location, it continues to provide quality education through dedicated faculty and industry collaborations.</div>
+
+        <div className="image">
+        <img src="https://img.jagranjosh.com/images/2022/April/1842022/Campus%20View%20of%20National%20Institute%20of%20Technology%20Uttarakhand_Campus-View.jpg" alt="Connect to Internet"/>
+        </div>
+
+    </div>
+  </div>
+  )
+}
+export default About_NIT;
